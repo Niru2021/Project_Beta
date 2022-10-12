ad34b5a4f88d94e0d581595b61014da2270e7a64
Roamin <romain.tessier1@epsi.fr>
2022-10-12 17:15:47 +0200

    modified analyse.php

--- web/analyse.php
+++ web/analyse.php
@@ -1,2 +1,28 @@
-<?php
-include("commun/header.php");
+<?php include("commun/header.php");?>
+<main>
+    <h1>Dashboard</h1>
+
+    <div class="insights">
+        <div class="employer">
+            <img >
+            <span class="material-icons-sharp">Employer</span>
+            <div class="middle">
+                <div class="left">
+                    <h3>Total employer</h3>
+                    <h1>10 300</h1>
+                </div>
+                <div class="progress">
+                    <svg>
+                        <circle cx="38" cy="38" r="36">
+
+                        </circle>
+                    </svg>
+                    <div class="number">
+                        <p>81%</p>
+                    </div>
+                </div>
+                <small class="text-muted">Effectif 2022</small>
+            </div>
+        </div>
+    </div>
+</main>
