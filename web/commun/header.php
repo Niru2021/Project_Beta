9049654333a61895f6120e0b6c010fb859716bee
Niru2021 <jeyakumn@gmail.com>
2022-10-12 15:33:12 +0200

    template 1

---
+++ web/commun/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <link rel="stylesheet" href="style/style.css">
+            <link rel="icon" href="img/favicon.png" />
+            <script type="text/javascript" src="javascript/central.js"></script>
+            <title>RH Analytics</title>
+        </head>
+        <body>  
+        <div class="barre_du_haut">    
+            <div class="barre_gauche">
+                <img src="img/logo_CLAAS.jpg" onclick='document.location.href = "index.php"'></img>
+            </div>
+            <div class="barre_droit">  
+                <table>
+                    <tr>
+                    <td>
+                    <select name="type_compte" id="type_compte" onchange='type_compte()'>
+                        <option value="1">Mon profil</option>
+                        <option value='2'>Emploie</option>
+                        <option value='3'>Formation</option>
+                        <option value='4'>Analyse</option>
+                    </select>
+                    </td>
+                    <td>
+                    <div id="nom_prenom">Jean-Paul GAUTHIER</div>
+                    </td>
+                    <td>
+                        <a href="deconnexion.php">
+                            <div class="log-out">
+                            </div>
+                        </a>
+                    </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
