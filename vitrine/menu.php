8a5b20aee6435027ec0ad5cc7f0883f9ccc8bac7
peewee750xv <khalissa.ahmed75@gmail.com>
2022-10-12 10:41:25 +0200

    MODIF MENU

--- vitrine/menu.php
+++ vitrine/menu.php
@@ -7,26 +7,12 @@
 				<ul>
 					<li class="dropdown">
 						<button class="dropbtn"><a href="colombo.php">PRODUIT</a></button>
-					<div class="dropdown-content">
-						<a href="colombo-musee-national.php">Musée National</a>
-						<a href="colombo-galle-face-green.php">Galle Face Green</a>
-						<a href="colombo-viharamahadevi-park.php">Viharamahadevi Park</a>
-					</div>
 					</li>
 					<li class="dropdown">
 						<button class="dropbtn"><a href="nord.php">SOLUTION</a></button>
-					<div class="dropdown-content">
-						<a href="nord-jaffna.php">Les données exploitable</a>
-						<a href="nord-point-pedro.php">Comment les exploiter</a>
-					</div>
 					</li>
 					<li class="dropdown">
 						<button class="dropbtn"><a href="sud.php">PERPECTIVE</a></button>
-					<div class="dropdown-content">
-						<a href="sud-trincomalai.php">Trincomalai</a>
-						<a href="sud-kandy.php">Kandy</a>
-						<a href="sud-sigiriya.php">Sigirîya</a>
-					</div>
 					</li>
 					<li class="dropdown">
 						<button class="dropbtn"><a href="historique.php">NOUS CONTACTER</a></button>
