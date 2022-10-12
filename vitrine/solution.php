140716cbdd100b510de678a4882c4a93d7ca9cd3
peewee750xv <khalissa.ahmed75@gmail.com>
2022-10-12 15:23:08 +0200

    MAPS

--- vitrine/solution.php
+++ vitrine/solution.php
@@ -42,8 +42,6 @@
 			</div>	
 		</div>
 	</div>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1008315.0843135094!2d79.74700006798909!3d9.178738373417994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afdb5e45096324b%3A0x7dd583a640795aa4!2sProvince%20du%20Nord%2C%20Sri%20Lanka!5e0!3m2!1sfr!2sfr!4v1572581651656!5m2!1sfr!2sfr" allowfullscreen="">
-	</iframe>
 	<?php include("pied.php");?>
   </body>
 </html>
