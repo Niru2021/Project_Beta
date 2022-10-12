fd151de6e48af33a6f0483587d785e1888797fe1
Niru2021 <jeyakumn@gmail.com>
2022-10-12 10:53:15 +0200

    test2

--- vitrine/index.php
+++ vitrine/index.php
@@ -18,7 +18,7 @@ id="gmap_canvas" src="https://maps.google.com/maps?q=epsi%20puteaux&t=&z=13&ie=U
 	<section id="boxes">
 		<div class="box">
 			<img src="img/stat.jpg" >
-			<h1>RH Analytics</h1>
+			<h1>RH lalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
 			<p>
 			<br>RH Analytics est une solution permettant d'analiser les données SIRH afin d'améliorer l'évolutions des employés et de leurs donner un meilleur cadre de travail.
 			<br>
